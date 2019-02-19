3aaa9d32eedaf4e47043e7a340886e16ae872853
R45H <gmr45h@gmail.com>
2019-02-19 09:56:03 +0300

    Add fancybox block

--- app/src/libs.js
+++ app/src/libs.js
@@ -2,3 +2,4 @@
 //=require '../libs/slick-carousel/slick/slick.js'
 //=require '../libs/svg4everybody/dist/svg4everybody.js'
 //=require '../libs/jquery.maskedinput/dist/jquery.maskedinput.js'
+//=require '../libs/fancybox/dist/jquery.fancybox.js'
